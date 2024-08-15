30a71a1505566582a4527af5202d9aa12e7bec73
Laquita Childress <LChildress@tql.com>
2024-08-15 09:59:46 -0400

    copied from craigs repo

--- cinemacast-client/src/actors/Actor.ts
+++ cinemacast-client/src/actors/Actor.ts
@@ -2,6 +2,9 @@ export class Actor {
   id: number | undefined;
   name = "";
   imdbID = "";
+  nationality = "";
+  gender = "";
+  awards: string[] = [];
 
   get isNew(): boolean {
     return this.id === undefined;
@@ -10,9 +13,10 @@ export class Actor {
   constructor(initializer?: any) {
     if (!initializer) return;
     if (initializer.id) this.id = initializer.id;
-    if (initializer.id) this.name = initializer.name;
-    if (initializer.id) this.imdbID = initializer.imdbID;
+    if (initializer.name) this.name = initializer.name;
+    if (initializer.imdbID) this.imdbID = initializer.imdbID;
+    if (initializer.nationality) this.nationality = initializer.nationality;
+    if (initializer.gender) this.gender = initializer.gender;
+    if (initializer.awards) this.awards = initializer.awards;
   }
 }
-
-
